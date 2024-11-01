302cf88f575fd5ae2b92e7677a867855b0f8f2c2
maxblowers <max@maxblowers.dev>
2024-11-01 14:56:47 -0700

    tailwindcss

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				"blowers": "#88ACEE",
+				"dark-black": "#272635",
+				"cream": "#F5F9E9",
+				"darker-black": "#0D1821"
+			}
+		},
+	},
+	plugins: [
+		require('@tailwindcss/typography'),
+		// ...
+	],
+
+}
